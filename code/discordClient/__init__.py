f55d486d69be40e8a546b49565c28978c7291f38
VidhishaKamat <vidhishakam@gmail.com>
2024-11-24 18:54:37 -0500

    display command added

--- code/discordClient/__init__.py
+++ code/discordClient/__init__.py
@@ -1,6 +1,6 @@
 import discord
 from discord import app_commands
-from .commands import ping, link, history, add, weekly
+from .commands import ping, link, history, add, weekly, display
 
 
 class DiscordClient(discord.Client):
@@ -22,6 +22,7 @@ class DiscordClient(discord.Client):
         await history.setup(self.tree)
         await add.setup(self.tree)
         await weekly.setup(self.tree)
+        await display.setup(self.tree)
 
     async def on_ready(self):
         print(f"Logged in as {self.user}")
